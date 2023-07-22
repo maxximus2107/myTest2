b73f5b4326df1997c73d7181eaa040b88b50d074
maxximus2107 <125027976+maxximus2107@users.noreply.github.com>
2023-07-21 22:09:12 -0500

    commited

---
+++ src/test/java/variousConcepts/GitTest2.java
@@ -0,0 +1,18 @@
+package variousConcepts;
+
+public class GitTest2 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("git test 2");
+	}
+	
+	public static void printMe() {
+		
+		System.out.println("git test1");
+		System.out.println("git test2");
+		System.out.println("git test3");
+		System.out.println("git test4");
+	}
+
+}
